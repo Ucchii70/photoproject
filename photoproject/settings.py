ea9a7a34c069e8b2ab29e0a7e2963a913524934c
Ucchii196 <137304583+Ucchii70@users.noreply.github.com>
2024-07-26 17:07:10 +0900

    Update settings.py

--- photoproject/settings.py
+++ photoproject/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-!-ct!00lquf(g742x3*xp+(9hpd%54f%9)=a))bi^g^yju*dcj"
+SECRET_KEY = "sample"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
